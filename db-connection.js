9f98ba2d04ad3f56481e24d773851050d3598c3a
Rakesh <rakesh22717@gmail.com>
2020-08-02 21:56:50 -0700

    added comments

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,15 @@
 var mysql = require('mysql');
 const { HOST, USER, PASSWORD, DATABASE } = require('./config');
 
+/*  
+We have used environment (.env extension) declarations to CONCEAL the sensitive 
+information. 
+HOST, USER, PASSWORD, DATABASE are being imported from config.js file which in turn
+import secret variables from .env file
+
+*/
+
+// connection setup
 var con = mysql.createConnection({
     host: HOST,
     user: USER,
@@ -8,8 +17,12 @@ var con = mysql.createConnection({
     database: DATABASE
 });
 
+// establish connection
 con.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
 });
 
 module.exports = con;
+
--- server.js
+++ server.js
@@ -1,64 +1,74 @@
-/* npm modules */
+/* ********************* npm modules *********************************************************** */
+
 var cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 var compression = require('compression')
-/* custom imports */
+
+/* ********************* custom imports ******************************************************** */
 const con = require('./db-connection.js')
-const { API_PASSWORD, PORT } = require('./config');
+
+/* ********** Secret information imports from .env through config.js file ********************** */
+const { PORT } = require('./config'); // PORT is being set to port 80; we are able to use port 80 with help of cors [as in certain environments inter-cors connections are not permitted by default]
 
 /* initializing expressJS */
 const app = express();
+
+/* Express compression middleware */
 app.use(compression()) // by default compress all responses
+
+/* parser especially for JSON as most of the exchange is of JSON type */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Enabling cors */
 app.use(cors())
 
-const port = process.env.PORT || 80;
+const port = PORT;
 
 /* Global declarations  */
 
-const messageNoAuth = "You are not authorized to use the API. Contact Admin to resolve the issue";
-const statusNoAuth = 405;
-const jsonNoAuth = []
-jsonNoAuth.push({
-  status: statusNoAuth,
-  result: messageNoAuth
-})
+/* Response nature informer */
+// const messageNoAuth = "You are not authorized to use the API. Contact Admin to resolve the issue";
+// const statusNoAuth = 405;  :::You are not authorized to use the API. Contact Admin to resolve the issue:::
 
-const statusSuccess = 200;
-const statusError = 403;
+const statusSuccess = 200; //  API successfully returns requested data in the result array.
+const statusError = 403; //  Something wrong with request inputs
 
 
-/* API LIST */
+/* API LIST: Names of the all APIs used in carbon print */
 
-const team = "/team"
-const food_1 = "/food/1"
+const team = "/team" // fetch list of team with designations
+const food_1 = "/food/1" // list of food items with respective carbon footprints
 const food_supply = "/food-supply"
-const eat_veg = "/eat-veg-carbon-footprints"
-const eat_local = "/eat-local-carbon-footprints"
-const carbon_historical_data = "/carbon-historical-data"
-const flights_carbon_footprint = "/flights-carbon-footprint"
-const temperature_historical_data = "/temperature-historical-data"
-const world_flight_carbon_footprints = "/world-flight-carbon-footprints"
+const eat_veg = "/eat-veg-carbon-footprints" // List of diet types with respective carbon footprints
+const eat_local = "/eat-local-carbon-footprints" // Carbon footprints of food supply chain resources
+const carbon_historical_data = "/carbon-historical-data" // Global average temperature increase from year 1850 to 2018
+const flights_carbon_footprint = "/flights-carbon-footprint" // List of flight class types with carbon footprint
+const temperature_historical_data = "/temperature-historical-data" // Carbon footprint from year 1850 to 2018
+const world_flight_carbon_footprints = "/world-flight-carbon-footprints" // Top 10 countries emitting highest carbon footprint
 
 /* Global functions */
-const matchPassword = (pass) => {
-  if (pass === password) {
-    return true
-  } else {
-    return false
-  }
-}
+/* password check is required for CREATE, UPDATE, and DELETE queries, but as of now our platform is not using such queries */
+// const matchPassword = (pass) => {
+//   if (pass === password) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 
-/************************  APIs  ********************************/
+/* *********************************************************************************************** */
+/**********************************  QUERIES  ******************************************************/
+/* *********************************************************************************************** */
 
 /* **************** Fetch Team Information ********************* */
 app.get(team, (req, res) => {
 
   const jsonMessage = []
-  con.query("SELECT t.*, d.designation_type FROM tb_team_members as t INNER JOIN tb_designation_type   as d ON t.designation_type_id = d.designation_type_id", function (err, result) {
+  con.query("SELECT t.*, d.designation_type FROM tb_team_members as t INNER JOIN tb_designation_type as d ON t.designation_type_id = d.designation_type_id", function (err, result) {
     if (err) {
+      // keeping in separate push, so that on client side we can know the nature of response before rendering any data [is to check whether API return data successfully or not; this pattern is followed in every API]
       jsonMessage.push({
         status: statusError
       })
@@ -297,4 +307,5 @@ app.get(food_supply, (req, res) => {
 
 });
 
-app.listen(80, () => console.log(`Listening on port ${port}`));
+// listening the port
+app.listen(port, () => console.log(`Listening on port ${port}`));
