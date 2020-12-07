92c01e272b8c1fc9d0c2863d92979ee09477c669
Rakesh <rakesh22717@gmail.com>
2020-12-06 22:38:40 -0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -6,7 +6,6 @@ We have used environment (.env extension) declarations to CONCEAL the sensitive
 information. 
 HOST, USER, PASSWORD, DATABASE are being imported from config.js file which in turn
 import secret variables from .env file
-
 */
 
 // NOTE: for server othher than WMDD, use exact IP(can be found at application page of WMDD) not a localhost 
